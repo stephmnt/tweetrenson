2da8af154788314161491110ef6df1115e1298be
Stéphane <stephane.manet@pm.me>
2020-02-16 18:19:52 +0100

    premier commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Générateur de tweet creux comme Renson pour collaborateur feignant
+
+Appuyer sur le bouton "générer". Refaite jusqu'à ce que vous soyez satisfait.
---
+++ main.js
@@ -0,0 +1,26 @@
+// Le bouton
+const ba = document.querySelector('#generer');
+let citation = document.querySelector('.tweet');
+
+// Elements de langage
+const texte1 = ["Aujourd'hui", "Désormais", "Dès à présent", "Plus que jamais"];
+const texte2 = ["c’est l’esprit de responsabilité qui doit primer", "il est temps d'être à la hauteur des enjeux", "nous avons tous à saisir l'importance du moment", "nous devons être pleinement lucide"];
+const texte3 = ["pour tous", "pour chacun", "pour le bien commun", "pour la France"];
+const texte4 = ["Il exige", "Il impose", "Il nécessite", "Il implique"];
+const texte5 = ["le rassemblement", "la cohésion", "le sens du sacrifice", "l'esprit républicain"];
+const texte6 = ["de la majorité présidentielle", "du gouvernement", "de l'équipe parlementaire", "de la République"];
+const texte7 = ["des françaises et des français", "des Parisiens", "de ma carrière", "des citoyens"];
+const texte8 = ["J’y contribuerai", "Je serai présent", "Vous avez mon 06", "Comptez sur moi"];
+
+
+// Fonction pour choisir un element du tableau de facon aleatoire
+function aleatoire(arr) {
+    return arr[Math.floor(Math.random() * arr.length )];
+}
+
+// Actions du bouton
+ba.addEventListener('click', () => {
+// Action du bouton effacer
+citation.innerHTML = aleatoire(texte1)+ ", " + aleatoire(texte2)+ ", " + aleatoire(texte3)+ ".<br /><br />" + aleatoire(texte4)+ " " + aleatoire(texte5)+ ". <br /><br />C'est dans l'intérêt " + aleatoire(texte6)+ ", mais surtout dans l'intérêt " + aleatoire(texte7)+ ".<br /><br />" + aleatoire(texte8)+ ".";
+});
+
