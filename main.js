fda8baf6a928e5ba59de6e38adbdbac598c4b989
Stéphane <stephane.manet@pm.me>
2020-02-18 10:26:45 +0100

    phrases supplémentaires

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ var citation = document.querySelector('.tweet');
 
 
 // Elements de langage
-const texte1 = ["Aujourd'hui", "Désormais", "Dès à présent", "Plus que jamais"];
+const texte1 = ["Aujourd'hui", "Désormais", "Dès à présent", "Plus que jamais", "Dorénavant", "Dès maintenant"];
 const texte2 = ["c’est l’esprit de responsabilité qui doit primer", "le temps est venu d'être à la hauteur des enjeux", "nous avons tous à saisir l'importance du moment", "nous devons avoir le tempérament adapté"];
-const texte3 = ["pour tous", "pour chacun", "pour le bien commun", "pour la France"];
-const texte4 = ["Il exige", "Il impose", "Il nécessite", "Il implique"];
-const texte5 = ["le rassemblement", "la cohésion", "le sens du sacrifice", "l'esprit républicain"];
-const texte6 = ["de la majorité présidentielle", "du gouvernement", "de ma carrière", "de la République"];
-const texte7 = ["des françaises et des français", "des Parisiens", "des femmes et des hommes", "des citoyens"];
-const texte8 = ["J’y contribuerai", "Je serai présent", "Vous avez mon 06", "Comptez sur moi"];
+const texte3 = ["pour tous", "pour chacun", "pour le bien commun", "pour la France", "pour les français", "pour l'avenir"];
+const texte4 = ["Il exige", "Il impose", "Il nécessite", "Il implique", "Il demande", "Il appelle"];
+const texte5 = ["le rassemblement", "la cohésion", "le sens du sacrifice", "l'esprit républicain", "la solidarité", "le sens du devoir"];
+const texte6 = ["de la majorité présidentielle", "du gouvernement", "de ma carrière", "de la République", "de la France", "de notre histoire"];
+const texte7 = ["des françaises et des français", "des Parisiens", "des femmes et des hommes", "des citoyens", "de nos concitoyens", ];
+const texte8 = ["J’y contribuerai", "Je serai présent", "Vous avez mon 06", "Comptez sur moi", "Je serai avec vous", "Vous avez mon engagement"];
 
 
 // Fonction pour choisir un element du tableau de facon aleatoire
@@ -29,9 +29,3 @@ ba.addEventListener('click', () => {
 	var a = citation.innerHTML;
 	document.getElementsByClassName('twitter-share-button')[0].setAttribute("data-text", a);
 
-// Dorénavant, dès maintenant
-// 
-// pour les français, pour l'avenir
-// Il demande, Il appelle
-// la solidarité, le sens du devoir
-// 
